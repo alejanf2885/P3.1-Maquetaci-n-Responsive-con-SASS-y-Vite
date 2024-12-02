b83e6a5678986569de06ef57d399599b9279a83d
alejanf2885 <alumno.648000@ies-azarquiel.es>
2024-12-02 20:37:17 +0100

    commit final

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src",
   build: {
-    outDir: "../docs", 
+    outDir: "../docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        gama: resolve(__dirname, "src/gama.html"),
+        contacta: resolve(__dirname, "src/contacta.html"),
+      },
+    },
   },
 });
